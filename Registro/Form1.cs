6e162cedef2020463477afe23123ac7d7aafbf95
Diana <gonzalezvasquezdianaitzel@gmail.com>
2025-03-20 20:28:38 -0600

    redondeado de botón guardar

--- Registro/Form1.cs
+++ Registro/Form1.cs
@@ -28,20 +28,36 @@ namespace Registro
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            txtClientes.BackColor = Color.White;
-            txtControl.BackColor = Color.White;
-            txttelefono.BackColor = Color.Yellow;
-            txtCorreo.BackColor = Color.White;
-            txtPin.BackColor = Color.White;
+            // Definir colores
+            Color defaultColor = Color.White;
+            Color highlightColor = Color.LightBlue;
+
+            // Restablecer todos los TextBox al color por defecto
+            txtClientes.BackColor = defaultColor;
+            txtControl.BackColor = defaultColor;
+            txttelefono.BackColor = defaultColor;
+            txtCorreo.BackColor = defaultColor;
+            txtPin.BackColor = defaultColor;
+
+            // Resaltar solo el TextBox activo
+            if (sender is TextBox textBox)
+            {
+                textBox.BackColor = highlightColor;
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            txtClientes.BackColor = Color.Yellow;
-            txtControl.BackColor = Color.White;
-            txttelefono.BackColor = Color.White;
-            txtCorreo.BackColor = Color.White;
-            txtPin.BackColor = Color.White;
+            // Establecer el color de fondo para todos los TextBox
+            Color defaultColor = Color.White;
+            Color highlightColor = Color.LightBlue;
+
+            // Aplicar colores
+            txtClientes.BackColor = highlightColor;
+            txtControl.BackColor = defaultColor;
+            txttelefono.BackColor = defaultColor;
+            txtCorreo.BackColor = defaultColor;
+            txtPin.BackColor = defaultColor;
         }
 
         private void lbPin_Click(object sender, EventArgs e)
@@ -57,7 +73,8 @@ namespace Registro
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-           
+            AgregarBordeRedondeadoBoton(btnGuardar);
+
             if (string.IsNullOrWhiteSpace(txtClientes.Text))
             {
                 MessageBox.Show("El campo usuario no puede estar vacío.");
@@ -96,6 +113,30 @@ namespace Registro
         
         }
 
+        private void AgregarBordeRedondeadoBoton(Button button)
+        {
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 0; // Eliminar el borde predeterminado
+            button.Paint += (s, e) =>
+            {
+                int radio = 20; // Tamaño de las esquinas redondeadas
+                GraphicsPath path = new GraphicsPath();
+                path.AddArc(0, 0, radio, radio, 180, 90);
+                path.AddArc(button.Width - radio, 0, radio, radio, 270, 90);
+                path.AddArc(button.Width - radio, button.Height - radio, radio, radio, 0, 90);
+                path.AddArc(0, button.Height - radio, radio, radio, 90, 90);
+                path.CloseFigure();
+
+                button.Region = new Region(path);
+
+                using (Pen pen = new Pen(Color.Gray, 2))
+                {
+                    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    e.Graphics.DrawPath(pen, path);
+                }
+            };
+        }
+
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             // Aquí puedes agregar el código para cancelar la operación
@@ -110,31 +151,65 @@ namespace Registro
 
         private void txtControl_TextChanged(object sender, EventArgs e)
         {
-            txtClientes.BackColor = Color.White;
-            txtControl.BackColor = Color.Yellow;
-            txttelefono.BackColor = Color.White;
-            txtCorreo.BackColor = Color.White;
-            txtPin.BackColor = Color.White;
+            // Definir colores
+            Color defaultColor = Color.White;
+            Color highlightColor = Color.LightBlue;
+
+            // Restablecer todos los TextBox al color por defecto
+            txtClientes.BackColor = defaultColor;
+            txtControl.BackColor = defaultColor;
+            txttelefono.BackColor = defaultColor;
+            txtCorreo.BackColor = defaultColor;
+            txtPin.BackColor = defaultColor;
+
+            // Resaltar solo el TextBox activo
+            if (sender is TextBox textBox)
+            {
+                textBox.BackColor = highlightColor;
+            }
 
         }
 
         private void txtCorreo_TextChanged(object sender, EventArgs e)
         {
-            txtClientes.BackColor = Color.White;
-            txtControl.BackColor = Color.White;
-            txttelefono.BackColor = Color.White;
-            txtCorreo.BackColor = Color.Yellow;
-            txtPin.BackColor = Color.White;
+            // Definir colores
+            Color defaultColor = Color.White;
+            Color highlightColor = Color.LightBlue;
+
+            // Restablecer todos los TextBox al color por defecto
+            txtClientes.BackColor = defaultColor;
+            txtControl.BackColor = defaultColor;
+            txttelefono.BackColor = defaultColor;
+            txtCorreo.BackColor = defaultColor;
+            txtPin.BackColor = defaultColor;
+
+            // Resaltar solo el TextBox activo
+            if (sender is TextBox textBox)
+            {
+                textBox.BackColor = highlightColor;
+            }
         }
 
         private void txtPin_TextChanged(object sender, EventArgs e)
         {
-            txtClientes.BackColor = Color.White;
-            txtControl.BackColor = Color.White;
-            txttelefono.BackColor = Color.White;
-            txtCorreo.BackColor = Color.White;
-            txtPin.BackColor = Color.Yellow;
+            // Definir colores
+            Color defaultColor = Color.White;
+            Color highlightColor = Color.LightBlue;
+
+            // Restablecer todos los TextBox al color por defecto
+            txtClientes.BackColor = defaultColor;
+            txtControl.BackColor = defaultColor;
+            txttelefono.BackColor = defaultColor;
+            txtCorreo.BackColor = defaultColor;
+            txtPin.BackColor = defaultColor;
+
+            // Resaltar solo el TextBox activo
+            if (sender is TextBox textBox)
+            {
+                textBox.BackColor = highlightColor;
+            }
         }
+
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
