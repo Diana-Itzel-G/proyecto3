75c2a9ff6b377fd4d3919206067549545586432a
Karenn <karenggarcia00@gmail.com>
2025-04-12 09:13:51 -0600

    Implementación de hilos en Iniciar Sesión

--- iniciarSesion/Form1.cs
+++ iniciarSesion/Form1.cs
@@ -48,22 +48,44 @@ namespace iniciarSesion
                 txtUsuario.Focus();
                 return;
             }
-            if (string.IsNullOrWhiteSpace(txtContraseña.Text))
+            // Crear un nuevo hilo para simular validación de usuario
+            Thread validarUsuarioThread = new Thread(() =>
             {
-                MessageBox.Show("El campo numero de control no puede estar vacío.");
-                txtContraseña.Focus();
-                return;
-            }
+                // Simular espera como si se estuviera consultando una base de datos
+                Thread.Sleep(2000); // 2 segundos de espera
+
+                // Abrir el formulario de Agenda desde el hilo principal (UI)
+                this.Invoke((MethodInvoker)delegate
+                {
             FechaAct agenda1 = new FechaAct();
             agenda1.Show();
+                     });
+            });
+
+            validarUsuarioThread.Start(); // Iniciar hilo
+            
         }
 
         private void txtUsuario_TextChanged(object sender, EventArgs e)
         {
             txtUsuario.BackColor = Color.Yellow;
             txtContraseña.BackColor = Color.White;
-           
-        }
+            // Hilo para revertir el color después de 1 segundo
+               Thread t = new Thread(() =>
+
+               {
+                   Thread.Sleep(1000); // espera 1 segundo
+        
+                     // Volver al hilo principal para modificar la UI
+                     this.Invoke((MethodInvoker)(() =>
+                     { 
+                         txtUsuario.BackColor = Color.White;
+                     }));
+               });
+      
+                  t.Start(); // Iniciar el hilo
+        }       
+  
 
         private void txtContraseña_TextChanged(object sender, EventArgs e)
         {
