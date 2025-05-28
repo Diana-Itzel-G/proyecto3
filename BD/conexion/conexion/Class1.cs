191ab7388bba1f62029779823f139893f9f9c18a
Diana <gonzalezvasquezdianaitzel@gmail.com>
2025-05-28 13:42:27 -0600

    Base de datos

---
+++ BD/conexion/conexion/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace conexion
+{
+    public class Conexion
+    {
+        private string server = "localhost";
+        private string database = "enfocatec";
+        private string user = "root";
+        private string password = "";
+        private string cadenaConexion;
+        private MySqlConnection conexion;
+
+        public Conexion()
+        {
+            cadenaConexion = $"Server={server};Database={database};Uid={user};Pwd={password};";
+            conexion = new MySqlConnection(cadenaConexion);
+        }
+
+        public MySqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
+
